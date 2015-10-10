df1208c0db5640dbed6e175f5a7f0754a5880c35
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2015-10-10 16:05:51 -0700

    participant-make a create method that saves the preference of the participant

--- restaurant_roulette/app/controllers/preferences_controller.rb
+++ restaurant_roulette/app/controllers/preferences_controller.rb
@@ -7,9 +7,14 @@ class PreferencesController < ApplicationController
 
   def create
     render plain: params[:preference].inspect
+    @preference = Preference.new(preference_params)
+    @preference.participant_id = current_user.id
+    @preference.event_id = params[:event_id]
 
-    @preference = Preference.new(params[:preference])
-
-    @preference.save
   end
+
+  private
+    def preference_params
+      params.require(:preference).permit(:price, :cuisine, :good_for_groups, :vegetarian, :distance)
+    end
 end
--- restaurant_roulette/config/routes.rb
+++ restaurant_roulette/config/routes.rb
@@ -18,7 +18,9 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :preferences
+  resources :events do
+    resources :preferences
+  end
   # Example resource route with options:
   #   resources :products do
   #     member do
