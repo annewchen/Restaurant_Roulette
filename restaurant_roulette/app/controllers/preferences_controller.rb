136e4ebcc88f42cf2dff287f4bc04b35adf0ff05
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2015-10-10 16:15:28 -0700

    participant-create a render thank you page that the participant sees after submitting pref form

--- restaurant_roulette/app/controllers/preferences_controller.rb
+++ restaurant_roulette/app/controllers/preferences_controller.rb
@@ -6,11 +6,12 @@ class PreferencesController < ApplicationController
   end
 
   def create
-    render plain: params[:preference].inspect
     @preference = Preference.new(preference_params)
     @preference.participant_id = current_user.id
     @preference.event_id = params[:event_id]
-
+    if @preference.save
+      render "thank"
+    end
   end
 
   private
