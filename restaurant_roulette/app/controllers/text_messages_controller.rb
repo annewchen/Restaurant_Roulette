5b7397d6b67f8a4bfa6f6f47a2d6a96c00c710f2
Utsab Saha <email": null>
2015-10-10 12:39:05 -0700

    Integrate with Twilio ==> 'Hello World' message

--- restaurant_roulette/app/controllers/text_messages_controller.rb
+++ restaurant_roulette/app/controllers/text_messages_controller.rb
@@ -1,7 +1,12 @@
+require 'twilio-ruby'
+
 class TextMessagesController < ApplicationController
   def index
     p "*" * 20
     p "In text_messages index"
     p "*" * 20
+    @twilioVersionMsg = 'Hello World! Currently running version ' + Twilio::VERSION + \
+        ' of the twilio-ruby library.'
+    p "twilio message: #{@twilioVersionMsg}"
   end
 end
