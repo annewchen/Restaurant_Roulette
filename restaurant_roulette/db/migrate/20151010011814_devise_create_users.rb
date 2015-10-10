a0403accf4dce49e66e64e97f600cd7b38da5410
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2015-10-09 19:17:07 -0700

    register-add full name and phone number to migration

--- restaurant_roulette/db/migrate/20151010011814_devise_create_users.rb
+++ restaurant_roulette/db/migrate/20151010011814_devise_create_users.rb
@@ -5,6 +5,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      #Added custom fields
+      t.string :phone_number
+      t.string :full_name
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- restaurant_roulette/db/schema.rb
+++ restaurant_roulette/db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 20151010011814) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "phone_number"
+    t.string   "full_name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
