5f4d39de6addf0d4d670b84e0a79628ba665db41
Priyanka Saigal <email": null>
2015-10-10 13:31:44 -0700

    Yelp-Add gem for yelp api

---
+++ restaurant_roulette/config/initializers/yelp.rb
@@ -0,0 +1,8 @@
+require 'yelp'
+
+Yelp.client.configure do |config|
+  config.consumer_key = 'Lx7pRFGoykfkvjEVB_MEUw'
+  config.consumer_secret = 'Si0b4gSdXwMF6g8GQlhqeyQP4hE'
+  config.token = 'xMK7htIiqmlLMLzMTqqJoFuWIVwaVRrz'
+  config.token_secret = '_7xodqldPYQ9VUq7bnjXToYUcAs'
+end
