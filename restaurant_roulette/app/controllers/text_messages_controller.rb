12c1d45ecebd71336ee2d61a16cffb65bb13342a
Utsab Saha <email": null>
2015-10-10 17:45:05 -0700

    Add method for texting participants; Add method for texting final selection to planner

--- restaurant_roulette/app/controllers/text_messages_controller.rb
+++ restaurant_roulette/app/controllers/text_messages_controller.rb
@@ -5,9 +5,19 @@ class TextMessagesController < ApplicationController
     p "*" * 20
     p "In text_messages index"
     p "*" * 20
-    @twilioVersionMsg = 'Hello World! Currently running version ' + Twilio::VERSION + \
-        ' of the twilio-ruby library.'
-    p "twilio message: #{@twilioVersionMsg}"
+
+    # TODO: currently hardcoding this to the first event in our database.
+    # Remove this eventually when we integrate with the rest of the code flow.
+    current_event = Event.find(1)
+    # send_text_messages_to_invitees_and_planner(current_invite)
+    send_final_selection_to_planner(current_event)
+
+  end
+
+  private
+
+  def send_text_messages_to_invitees_and_planner(event)
+    p "in send_text_messages_to_invitees_and_planner "
 
     account_sid = "AC6882f6d03c767dfed68ddd6e61554b76"
     auth_token = "b6c51acfab9f2d0ceefee450d074cc2c"
@@ -15,20 +25,42 @@ class TextMessagesController < ApplicationController
 
     from = "+15105008394" # Your Twilio number
 
-    friends = {
-    "+19097628390" => "Anne",
-    "+15103043300" => "Utsab",
-    "+16508787883" => "Gabby",
-    "+14085134453" => "Priyanka"
-    }
-
-    friends.each do |key, value|
+    # send message to invitees
+    event.invitations.each do |invitation|
       client.account.messages.create(
         :from => from,
-        :to => key,
-        :body => "Hey #{value}, We're going out for food. Fill out this form: http://restaurant-roullete.heroku.com/events/143"
+        :to => invitation.phone_number,
+        :body => "Hey #{invitation.full_name}, We're going out for food. Fill out this form: http://restaurant-roullete.heroku.com/events/143"
       )
-      puts "Sent message to #{value}"
+      puts "Sent invitation message to invitee: #{invitation.full_name}"
     end
+
+    # send message to planner
+    client.account.messages.create(
+      :from => from,
+      :to => event.planner.phone_number,
+      :body => "Hey #{event.planner.full_name}, We're going out for food. Fill out this form: http://restaurant-roullete.heroku.com/events/143"
+    )
+    puts "Sent invitation message to planner: #{event.planner.full_name}"
   end
+
+  def send_final_selection_to_planner(event)
+    p "in send_final_selection_to_planner "
+
+    account_sid = "AC6882f6d03c767dfed68ddd6e61554b76"
+    auth_token = "b6c51acfab9f2d0ceefee450d074cc2c"
+    client = Twilio::REST::Client.new account_sid, auth_token
+
+    from = "+15105008394" # Your Twilio number
+
+    # send message to planner
+    client.account.messages.create(
+      :from => from,
+      :to => event.planner.phone_number,
+      :body => "Selected restaurant: #{event.selected_restaurant}"
+    )
+    puts "Sent final selection message to planner: #{event.planner.full_name}"
+  end
+
+
 end
