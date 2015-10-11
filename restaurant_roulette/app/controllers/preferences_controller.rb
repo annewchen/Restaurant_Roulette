edc018970e810d8e4b442d16e0e16b14b5409707
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2015-10-10 17:59:54 -0700

    participant-edit create method that only takes in 2 filters

--- restaurant_roulette/app/controllers/preferences_controller.rb
+++ restaurant_roulette/app/controllers/preferences_controller.rb
@@ -3,19 +3,38 @@ class PreferencesController < ApplicationController
   end
 
   def new
+    @preference = Preference.new
   end
 
   def create
     @preference = Preference.new(preference_params)
-    @preference.participant_id = current_user.id
-    @preference.event_id = params[:event_id]
-    if @preference.save
-      render "thank"
+    other_params
+    values = [@preference.price, @preference.cuisine, @preference.good_for_groups , @preference.vegetarian, @preference.distance]
+
+    filter_count = 0
+
+    values.each do |value|
+      if value != "" && value != nil
+        filter_count+=1
+      end
+    end
+
+    if filter_count <= 2 && @preference.save
+        render "thank"
+    else
+        render "index"
     end
   end
 
   private
     def preference_params
-      params.require(:preference).permit(:price, :cuisine, :good_for_groups, :vegetarian, :distance)
+      params.require(:preference).permit(:price, :cuisine, :distance)
+    end
+
+    def other_params
+      @preference.participant_id = current_user.id
+      @preference.event_id = params[:event_id]
+      @preference.good_for_groups = params[:good_for_groups]
+      @preference.vegetarian = params[:vegetarian]
     end
 end
