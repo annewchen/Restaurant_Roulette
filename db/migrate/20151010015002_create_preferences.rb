fe4f851fcfa75df6e6db2a5702526ad8a17761db
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2015-10-11 17:47:09 -0700

    participant-view-edit choices and logic in choosing preferences

--- db/migrate/20151010015002_create_preferences.rb
+++ db/migrate/20151010015002_create_preferences.rb
@@ -2,7 +2,7 @@ class CreatePreferences < ActiveRecord::Migration
   def change
     create_table :preferences do |t|
       t.boolean :is_fancy
-      t.string :cuisine
+      t.string :cuisine, :default => nil
       t.boolean :is_vegetarian
       t.integer :distance
       t.timestamps null: false
