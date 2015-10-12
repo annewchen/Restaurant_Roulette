b15749e9f54075eb627e1402b928d3d19fcfa5f6
Anne Chen <annewchen@gmail.com>
2015-10-11 22:11:39 -0700

    Integrate with Twilio.  Send final restaurant selection to planner

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,3 +1,5 @@
+require "yelp_helper"
+
 class PreferencesController < ApplicationController
   def index
   end
@@ -37,7 +39,7 @@ class PreferencesController < ApplicationController
     end
 
  # CHANGE BACK TO ==
-    if event.preferences.count == (event.invitations.count + 1)
+    if event.preferences.count >= (event.invitations.count + 1)
       decision_algorithm(event)
     end
   end
@@ -98,11 +100,16 @@ class PreferencesController < ApplicationController
       p "*" * 20
       p "all choices: #{all_choices}"
       all_choices
-       #selected_restaurant_hash = YelpHelper.ping_yelp(all_choices["is_fancy"], all_choices["cuisine"], all_choices["distance"], all_choices["is_vegetarian"], event.street_address)
+      selected_restaurant_hash = YelpHelper.ping_yelp(all_choices["is_fancy"], all_choices["cuisine"], all_choices["distance"], all_choices["is_vegetarian"], event.street_address)
 
+      p "selected restaurant: #{selected_restaurant_hash}"
 
-      # event.selected_restaurant = selected_
-      # send text message
+      event.selected_restaurant_name = selected_restaurant_hash["name"]
+      event.selected_restaurant_address = selected_restaurant_hash["address"]
+
+      event.save
+
+      TextMessagesHelper.send_final_selection_to_planner(event)
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,10 +18,12 @@ ActiveRecord::Schema.define(version: 20151010015022) do
 
   create_table "events", force: :cascade do |t|
     t.string   "street_address"
-    t.string   "selected_restaurant"
+    t.string   "selected_restaurant_name"
+    t.string   "selected_restaurant_address"
+    t.string   "selected_restaurant_phone_number"
     t.integer  "planner_id"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
   end
 
   create_table "invitations", force: :cascade do |t|
