f6bc07ebb707ec090145ba43b26d7a41b7f7e4c8
Anne Chen <annewchen@gmail.com>
2015-10-12 21:22:24 -0700

    Planner View: add delete button per guest added

--- app/assets/javascripts/event.js
+++ app/assets/javascripts/event.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var newForm = "<div class = 'invitationForm'><p>Name: <input type='text' name='full_name[]' placeholder='John Doe'></input><br>Phone Number: <input type='text' name='phone_number[]' placeholder='xxx-xxx-xxxx'></input><br></p></div>";
+  var newForm = "<div class = 'invitationForm'><p>Name: <input type='text' name='full_name[]' placeholder='John Doe'></input><br>Phone Number: <input type='text' name='phone_number[]' placeholder='xxx-xxx-xxxx'></input><button id='deleteForm'> Delete Guest</button><br></p></div>";
   $('.addInvitation').click(function(){
 
     $('.invitationSet').append(newForm);//.html());
