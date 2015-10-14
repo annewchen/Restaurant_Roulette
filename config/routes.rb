0c9ac0cbf5f3b8d0f70774588d54b55139e116c3
Utsab Saha <email": null>
2015-10-14 10:48:17 -0700

    css-log-register-take out forgot your password link and create home page as the root

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'events#index'
+  root 'welcome#index'
 
   resources :text_messages
 
